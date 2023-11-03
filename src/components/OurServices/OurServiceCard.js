41fc1e473f9d7911b95b9a58f24e37f734f80b08
Gbadass <theboyfakaa@gmail.com>
2023-11-02 17:06:13 -0700

    ourservice page

--- src/components/OurServices/OurServiceCard.js
+++ src/components/OurServices/OurServiceCard.js
@@ -5,15 +5,19 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 
-export default function ServicesCard(props) {
+export default function OurServicesCard(props) {
   return (
-    <Card className=" mb-5 border-0 border-top border-5 border-info " style={{height:"275px", width:"250px"}}>
+    <Card className=" mb-5 border-0 border-top border-5 border-info " style={{height:"350px", width:"auto"}}>
       <Card.Img variant="top" src={props.image} className="img-fluid mx-auto mt-4" style={{height:"95px", width:"95px"}}/>
       <Card.Body className="text-center">
         <Card.Title className="fs-2">{props.title}</Card.Title>
         <Card.Text className="fw-semibold text-secondary">
           {props.text}
         </Card.Text>
+        <Button variant="info" className="pricing-order-button border-0 mt-2 fw-bold btn-md text-white" size="lg" style={{ whiteSpace: 'nowrap' }}>
+        ORDER NOW
+      </Button>{' '}
+
       </Card.Body>
     </Card>
   );
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,12 +1,120 @@
 import MainNavbar from "../components/MainNavbar";
-import "../styles/Services.css"
+import "../styles/Services.css";
+import { FaChevronRight } from "react-icons/fa";
+import servicebg from "../assets/images/service-bg.png";
+import { Container, Col, Row, Card } from "react-bootstrap";
+import OurServicesCard from "../components/OurServices/OurServiceCard";
+import washingmachineImg from "../assets/images/washing-icon.png";
+import ironImg from "../assets/images/iron-icon.png";
+import foldClothImg from "../assets/images/wash-fold-icon.png";
+import dryClean from "../assets/images/dry-clean-icon.png";
+import steamIron from "../assets/images/steam-iron-icon.png";
+import CurtainsWash from "../assets/images/curtains-icon.png";
+import whyUsImg from "../assets/images/why-us-img.png";
+import Button from "react-bootstrap/Button";
+import Footer from "../components/Footer";
 
 function Services() {
-  return(
+  return (
     <div>
-<MainNavbar/>
+      <MainNavbar />
+      <div>
+        <div className="below-navbar container-fluid d-flex pt-4 pb-3">
+          <div className="container d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <p className="fs-5 text-white">Home</p>
+              <FaChevronRight className="greatherthan-Icon" />
+              <p className="fs-5 text-white">Pricing</p>
+            </div>
+            <p className="fs-3  text-white">Services</p>
+          </div>
+        </div>
+      </div>
+      <Container fluid className="servicebg-container">
+        <Col className="service-title-div align-center pt-5 pb-5">
+          <p className="title-ptag fs-5">Stay Clean And Dress Well</p>
+          <h1 className=" fw-bold">Our Services</h1>
+        </Col>
+        <Container>
+          <Row className="gap-0 ">
+            <Col xs={12} md={6} lg={4}>
+              <OurServicesCard
+                image={washingmachineImg}
+                title={"Washing"}
+                text={"We wash your clothes and prep them to iron"}
+              />
+            </Col>
+            <Col xs={12} md={6} lg={4}>
+              <OurServicesCard
+                image={ironImg}
+                title={"Iron"}
+                text={"We wash your clothes and prep them to iron"}
+              />
+            </Col>
+            <Col xs={12} md={6} lg={4}>
+              <OurServicesCard
+                image={foldClothImg}
+                title={"Wash and Fold"}
+                text={"We wash your clothes and Fold for ironing"}
+              />
+            </Col>
+          </Row>
+          <Row className="gap-0 ">
+            <Col xs={12} md={6} lg={4}>
+              <OurServicesCard
+                image={dryClean}
+                title={"Washing"}
+                text={"We wash your clothes and prep them to iron"}
+              />
+            </Col>
+            <Col xs={12} md={6} lg={4}>
+              <OurServicesCard
+                image={steamIron}
+                title={"Iron"}
+                text={"We wash your clothes and prep them to iron"}
+              />
+            </Col>
+            <Col xs={12} md={6} lg={4} className="md-align-center">
+              <OurServicesCard
+                image={CurtainsWash}
+                title={"Wash and Fold"}
+                text={"We wash your curtains and Fold Them"}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container className="pt-5">
+        <Row>
+          <Col  xs={12} md={6}>
+            <Card className="bg-Img_Card border-0 mt-5">
+              <img src={whyUsImg} className="image-fluid" alt="" />
+            </Card>
+          </Col>
+          <Col xs={12}  md={6} className=" mt-5">
+            <Card  className="bg-Img_Card border-0 mt-5">
+              <h1 className="fw-bold">Why Choose Us</h1>
+              <p className="w-75">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commo do consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <div>
+                <Button variant="info" className=" text-white" size="lg">
+                  Large button
+                </Button>{" "}
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Footer/>
     </div>
-  )
+  );
 }
 
 export default Services;
